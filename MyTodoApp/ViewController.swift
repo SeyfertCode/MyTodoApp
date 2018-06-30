add17c11bb98feaaa85958ef746df44702d16e78
Usuario invitado <Guest@mac-mini-de-jorge.local>
2018-06-30 14:52:34 -0500

    Initial Commit

---
+++ MyTodoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTodoApp
+//
+//  Created by Usuario invitado on 30/6/18.
+//  Copyright © 2018 DoApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
