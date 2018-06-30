d2632483302bbdfb326db7fd8956985711297f29
Usuario invitado <Guest@Mac-mini-de-Jorge.local>
2018-06-30 16:32:23 -0500

    base project

---
+++ MyTodoApp/Controllers/SignInViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SignInViewController.swift
+//  MyTodoApp
+//
+//  Created by Usuario invitado on 30/6/18.
+//  Copyright © 2018 DoApps. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var SignInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = "Bienvenido"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func sigInAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToHome", sender: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
